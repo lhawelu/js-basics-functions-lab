2dd1acce49da3fe7a5056edf29670a31b8f6dfc5
Larry Hawelu <lhawelu@gmail.com>
2021-02-22 19:45:23 -0800

    commit tree 7c31b5486e7ef5c19716ada26678d2c763879abb

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue){
+  const distanceBlocks = Math.abs((someValue - 43) + 1);
+  return distanceBlocks
+}
+
+function distanceFromHqInFeet(someValue){
+  const distanceFeet = distanceFromHqInBlocks(someValue) * 264;
+  return distanceFeet
+}
+
+function distanceTravelledInFeet(startingBlock, endBlock){
+  const totalFeet = (Math.abs(startingBlock - endBlock)) * 264
+  return totalFeet
+}
+
+function calculatesFarePrice(startingBlock, endBlock){
+    const totalFeet = (Math.abs(startingBlock - endBlock)) * 264
+    if (totalFeet < 400) {
+      const totalCost = 0
+      return totalCost
+    } else if (totalFeet <= 2000 ) {
+      const totalCost = (totalFeet -400) * .02
+      return totalCost
+    } else if (totalFeet < 2500 ) {
+      const totalCost = 25
+      return totalCost
+    } else {
+      return "cannot travel that far"
+    }
+}
